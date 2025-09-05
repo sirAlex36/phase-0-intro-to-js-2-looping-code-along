2fbecc35583f843a4f334e520f1c3bb80cc63063
SirAlex36 <alexngenoo@gmail.com>
2025-09-05 08:20:43 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const names=["Charlie", "Samip", "Ali"];
+const event="birthday";
+
+function writeCards(names, event) {
+  let messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return messages;
+}
+console.log(writeCards(names, event));
+
+function countDown(number) {  
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
+countDown(10);  
